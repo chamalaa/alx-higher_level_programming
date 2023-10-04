9aff1ebcb7f7bab99bc4d6585ec1a6c4e4b44f59
chamalaa <laachaimae95@gmail.com>
2023-10-04 11:25:25 -0700

    ifelse

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-
-for dig1 in range(0, 10):
-    for dig2 in range(dig1 + 1, 10):
-        if dig1 == 8 and dig2 == 9:
-            print("{}{}".format(dig1+1, dig2))
-        else:
-            print("{}{}".format(dig1, dig2), end=", ")
+for i in range(0, 8):
+    for j in range(i + 1, 10):
+        print("{:d}{:d}".format(i, j), end=', ')
+print("{:d}{:d}".format(i + 1, j))
